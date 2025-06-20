83171c2430ac152440105c5146b751805b066f7d
Youdahe Asfaw <youdaheasfaw@gmail.com>
2025-06-20 15:44:31 -0500

    removed names and added flags

--- front_end/index.js
+++ front_end/index.js
@@ -3,14 +3,15 @@
 function clicked(){
     const lon = document.getElementById('long').value
     const lat = document.getElementById('lang').value
+    const image = document.getElementById('flag')
     
     fetch(`http://localhost:3000/?lon=${lon}&lat=${lat}`)
     .then(res => res.json())
     .then(data => {
-        const word = document.getElementById('name')
-        word.textContent = `Country : ${data.name}`
         const temp = document.getElementById('temp')
         temp.textContent = `Temperature : ${data.temp}`
+        image.src = `https://flagsapi.com/${data.name}/flat/64.png`
+        image.style.display = 'block'
     })
     .catch(err => console.log(err))
 }
